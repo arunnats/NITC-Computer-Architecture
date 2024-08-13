d21951ecf0dd1f7f88b25b53471b77948674fcaa
arunnats <arunnats2004@gmail.com>
2024-08-13 13:30:40 +0530

    almost done with top level code

---
+++ Multicycle/topmult.v
@@ -0,0 +1,10 @@
+topmulti.v
+module topmulti(input clk, reset,
+    output [31:0] writedata, adr,
+    output memwrite
+);
+    wire [31:0] readdata;
+
+    mips mips(clk, reset, adr, writedata, memwrite, readdata);
+    mem mem(clk, memwrite, adr, writedata, readdata);
+endmodule 
