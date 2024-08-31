eeb0609d428ba2eceb9e8492fa65687dcd52fe29
arunnats <arunnats2004@gmail.com>
2024-08-31 23:32:46 +0530

    implemented pipeline

---
+++ Multicycle/Testbenches/Pipeline/mux2.v
@@ -0,0 +1,11 @@
+module mux2 #(parameter WIDTH = 8) (
+    input [WIDTH-1:0] d0, d1,
+    input s,
+    output reg [WIDTH-1:0] y
+);
+    always @( * )
+        case(s)
+            1'b0: y <= d0;
+            1'b1: y <= d1;
+        endcase
+endmodule 
---
+++ Multicycle/Testbenches/Pipeline/topmult.v
@@ -0,0 +1,9 @@
+module topmulti(input clk, reset,
+    output [31:0] writedata, adr,
+    output memwrite
+);
+    wire [31:0] readdata;
+
+    mips mips(clk, reset, adr, writedata, memwrite, readdata);
+    mem mem(clk, memwrite, adr, writedata, readdata);
+endmodule 
